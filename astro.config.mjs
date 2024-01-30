29a07ce464a5fa7217c4beaad6503a2a56593cdc
rinuwaii <fiteuwu@outlook.com>
2024-01-30 15:33:16 -0500

    disabling oauth for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [decapCmsOauth()],
+  integrations: [decapCmsOauth({oauthDisabled: true})],
   adapter: cloudflare()
 });
