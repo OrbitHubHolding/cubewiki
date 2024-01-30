cd0cfcb3b18f244e0badfb9a0bcdd7801e08178d
rinuwaii <fiteuwu@outlook.com>
2024-01-30 12:01:40 -0500

    add cloudflare ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import decapCmsOauth from "astro-decap-cms-oauth";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  integrations: [decapCmsOauth()]
+  output: "hybrid",
+  integrations: [decapCmsOauth()],
+  adapter: cloudflare()
 });
