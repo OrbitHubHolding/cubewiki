97b02baee9f1de722a6167a575ba5d104fb6493a
rinuwaii <fiteuwu@outlook.com>
2024-01-31 01:08:30 -0500

    crying

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   integrations: [decapCmsOauth()],
   adapter: cloudflare()
 });
